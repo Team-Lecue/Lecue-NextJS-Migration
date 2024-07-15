4fd3c9ced5cb1ad6e0f8a27481e00b42ee0f0765
jungwoo3490 <jungwoo3490@naver.com>
2024-07-15 16:02:37 +0900

    fix: svg 세팅 충돌 해결

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import ImgError from "../assets/img/img_error.svg";
+import * as S from "./ErrorPage.style";
+
 function NotFound() {
-  return <div>not-found</div>;
+  const handleClickHomeButton = () => {};
+
+  return (
+    <S.ErrorPageWrapper>
+      <ImgError />
+      <S.ErrorPageMessage>이런, 오류가 발생했어요</S.ErrorPageMessage>
+      <S.HomeButton onClick={handleClickHomeButton}>홈 화면으로</S.HomeButton>
+    </S.ErrorPageWrapper>
+  );
 }
 
 export default NotFound;
