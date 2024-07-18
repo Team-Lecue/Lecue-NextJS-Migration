a14b8a33c124aee39e67577f0d648e50ce4d307a
jungwoo3490 <jungwoo3490@naver.com>
2024-07-18 20:34:25 +0900

    feat: 세션스토리지 useEffect 적용

--- src/utils/savedScrollPosition.ts
+++ src/utils/savedScrollPosition.ts
@@ -1,5 +1,12 @@
+import { useEffect, useState } from "react";
+
 const useScrollPosition = () => {
-  const storedValue = sessionStorage.getItem('scrollPosition');
+  const [storedValue, setScrolledValue] = useState<string | null>(null);
+  useEffect(() => {
+    const storedScrollPosition = sessionStorage.getItem("scrollPosition");
+    setScrolledValue(storedScrollPosition);
+  }, []);
+
   const savedScrollPosition =
     storedValue !== null ? parseInt(storedValue, 10) : 0;
 
